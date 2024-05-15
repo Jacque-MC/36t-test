c1d290833d07fea453eb1a8432e001f640dbd785
Jacqueline Maldonado <j.mc_business@outlook.com>
2024-05-15 01:39:12 -0700

    Se agregó secret key a settings para pruebas más simples

--- escuela/settings.py
+++ escuela/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ex!rz1y2kta1@4u+18ahuoa61*v0@v_6h^$_$_m*x-4xmfoacv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
