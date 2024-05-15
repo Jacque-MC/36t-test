9fd9730cb43602f4f21fbbebd8162daa1017d933
Jacqueline Maldonado <j.mc_business@outlook.com>
2024-05-14 20:12:11 -0700

    Filtrar salones por código

--- entidades/views.py
+++ entidades/views.py
@@ -27,4 +27,19 @@ class SalonViewSet(ModelViewSet):
     serializer_class = SalonSerializer
 
     def get_queryset(self):
-        return Salon.objects.all().order_by('letra', 'codigo')
+        code = self.request.query_params.get('codigo', None)
+        salones = Salon.objects.all().order_by('letra', 'codigo')
+        if code:
+            salones = salones.filter(codigo__exact=code)
+        return salones
+
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'data':serializer.data, 'objects': queryset.count()})
