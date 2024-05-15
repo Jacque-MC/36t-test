8dfc8d361951d6fd9bad4b8cdaf9c7249e657c63
Jacqueline Maldonado <j.mc_business@outlook.com>
2024-05-14 19:26:48 -0700

    Eliminar maestro y sus salones

--- entidades/urls.py
+++ entidades/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('maestros/', MaestroViewSet.as_view({'get': 'list', 'post': 'create'})),
     # path('maestros/create/', MaestroViewSet.as_view({'post': 'create'})),
     # path('maestros/<int:pk>/edit/', MaestroViewSet.as_view({'put': 'partial_update'})),
-    path('maestros/<int:pk>/delete/', MaestroViewSet.as_view({'delete': 'destroy'})),
-    path('maestros/<int:pk>/', MaestroViewSet.as_view({'get': 'retrieve', 'put': 'partial_update'})),
+    # path('maestros/<int:pk>/delete/', MaestroViewSet.as_view({'delete': 'destroy'})),
+    path('maestros/<int:pk>/', MaestroViewSet.as_view({'get': 'retrieve', 'put': 'partial_update', 'delete': 'destroy'})),
     #
     path('salones/', SalonViewSet.as_view({'get': 'list', 'post': 'create'})),
     # path('salones/create/', SalonViewSet.as_view({'post': 'create'})),
