7430743587d98fdbee9c47ac2aad690db1d1753e
Jacqueline Maldonado <j.mc_business@outlook.com>
2024-05-14 19:39:20 -0700

    Validación: no más de tres salones por maestro al agregar o editar

--- entidades/serializers.py
+++ entidades/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from entidades.models import Maestro, Salon
 
 
@@ -21,6 +21,13 @@ class MaestroSerializer(serializers.ModelSerializer):
         model = Maestro
         fields = '__all__'
     
+    def validate_salones(self, data):
+        if len(data) > 3:
+            raise exceptions.ValidationError({
+                'error': 'No se puede agregar más de tres salones por maestro.'
+            })
+        return data
+    
     def create(self, validated_data):
         salones = validated_data.pop('salones')
         maestro = Maestro.objects.create(**validated_data)
@@ -34,8 +41,15 @@ class MaestroSerializer(serializers.ModelSerializer):
             setattr(instance, attr, value)
         instance.save()
         for salon in salones:
-            salon_instance = instance.salones.get(pk=salon['id'])
-            for attr, value in salon.items():
-                setattr(salon_instance, attr, value)
-            salon_instance.save()
+            if 'id' in salon:
+                salon_instance = instance.salones.get(pk=salon['id'])
+                for attr, value in salon.items():
+                    setattr(salon_instance, attr, value)
+                salon_instance.save()
+            elif len(instance.salones.all()) < 3:
+                Salon.objects.create(**salon, maestro=instance)
+            else:
+                raise exceptions.ValidationError({
+                    'error': 'No se puede agregar más de tres salones por maestro.'
+                })
         return instance
